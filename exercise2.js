4148701de7b925039d8ba2ee14ace7811885317d
helenmenge <89815725+helenmenge@users.noreply.github.com>
2021-11-05 04:57:40 -0500

    Add files via upload

---
+++ exercise2.js
@@ -0,0 +1,14 @@
+const isPrime = num => new Promise((resolve, reject) => {
+    setTimeout(function() {
+        for (let i = 2, s = Math.sqrt(num); i <= s; i++)
+            if (num % i === 0) reject({ prime: false });
+        resolve({ prime: num > 1 });
+    }, 500);
+});
+async function isPrimeAsync(num) {
+    let result = await isPrime(5);
+    console.log(result);
+}
+console.log('start');
+isPrimeAsync(7)
+console.log('end');
